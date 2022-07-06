8173f83ff014993b88bbe0aa8fac232057e0155d
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-07-06 18:05:28 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,14 +1,23 @@
-function countProps(object) {
-  let propCount = 0;
-  console.log(propCount);
+// Change code below this line
+function findMatches(...rest) {
+  const matches = []; // Don't change this line
+  let mass = [...rest];
+  console.log(...mass[0]);
+  for (const number of rest) {
+    console.log(number);
 
-  const values = Object.values(object);
-  console.log(values);
+    // const copyOfNumber = [...number];
+    // console.log(copyOfNumber);
 
-  propCount = values.length;
-  console.log(propCount);
+    // for (const numb of copyOfNumber) {
+    //   console.log(numb);
+    // }
+  }
 
-  return propCount;
+  // let numbers = [...number];
+  // console.log(numbers);
+
+  // Change code above this line
+  return matches;
 }
-
-countProps({ name: 'Mango', age: 2 });
+findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7);
