720328ca5d574cddee6c1829df99849b1986a6e2
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-07-14 11:31:53 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,22 +1,85 @@
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
-  // Change code below this line
+const arrayOfUsers = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39,
+  },
+];
+// Change code below this line
+const getFriends = users =>
+  users.flatMap(user => user.friends).filter((user, index, array) => array.indexOf(user) === index);
 
-  orderedItems.forEach(function (number)) {
-    console.log(number);
-  }
-    
+// Change code above this line
+console.log(getFriends(arrayOfUsers));
 
-  // Change code above this line
-  return totalPrice;
-}
-
-calculateTotalPrice([12, 85, 37, 4])
-
-
-
-
-const numbers = [5, 10, 15, 20, 25];
-numbers.forEach(function (number, index) {
-  console.log(number);
-});
+// const getFriends = users => {
+//   const allUsers = users.flatMap(user => user.friends);
+//   console.log(allUsers);
+//   const uniqueUsers = allUsers.filter((user, index, array) => array.indexOf(user) === index);
+//   console.log(uniqueUsers);
+// };
